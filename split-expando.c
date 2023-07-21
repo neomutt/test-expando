44cc78041447c68411afb59cfc650f5a30e30423
Richard Russon <rich@flatcap.org>
2023-07-22 00:44:07 +0100

    fix buffers

--- split-expando.c
+++ split-expando.c
@@ -40,13 +40,13 @@ bool callback_fruit(const char *option, void *data, struct Buffer *buf)
   struct Fruit *fruit = data;
   if (mutt_str_equal(option, "a"))
   {
-    mutt_buffer_addstr(buf, fruit->apple);
+    buf_addstr(buf, fruit->apple);
     return true;
   }
 
   if (mutt_str_equal(option, "b"))
   {
-    mutt_buffer_addstr(buf, fruit->banana);
+    buf_addstr(buf, fruit->banana);
     return true;
   }
 
@@ -58,13 +58,13 @@ bool callback_animal(const char *option, void *data, struct Buffer *buf)
   struct Animal *animal = data;
   if (mutt_str_equal(option, "c"))
   {
-    mutt_buffer_addstr(buf, animal->cat);
+    buf_addstr(buf, animal->cat);
     return true;
   }
 
   if (mutt_str_equal(option, "d"))
   {
-    mutt_buffer_addstr(buf, animal->dog);
+    buf_addstr(buf, animal->dog);
     return true;
   }
 
@@ -73,7 +73,7 @@ bool callback_animal(const char *option, void *data, struct Buffer *buf)
 
 void expando(struct Buffer *buf, const char *fmt, struct Option *callback)
 {
-  struct Buffer buf_callback = mutt_buffer_make(1024);
+  struct Buffer buf_callback = buf_make(1024);
 
   for (const char *ch = fmt; *ch; ch++)
   {
@@ -85,13 +85,13 @@ void expando(struct Buffer *buf, const char *fmt, struct Option *callback)
       {
         if (*ch == callback[i].option[0])
         {
-          mutt_buffer_reset(&buf_callback);
+          buf_reset(&buf_callback);
           result = callback[i].callback(callback[i].option, callback[i].data, &buf_callback);
           if (result)
           {
-            mutt_buffer_addstr(buf, "\033[1;32m");
-            mutt_buffer_addstr(buf, buf_callback.data);
-            mutt_buffer_addstr(buf, "\033[m");
+            buf_addstr(buf, "\033[1;32m");
+            buf_addstr(buf, buf_callback.data);
+            buf_addstr(buf, "\033[m");
           }
 
           break;
@@ -100,16 +100,16 @@ void expando(struct Buffer *buf, const char *fmt, struct Option *callback)
 
       if (!result)
       {
-        mutt_buffer_addch(buf, '%');
-        mutt_buffer_addch(buf, *ch);
+        buf_addch(buf, '%');
+        buf_addch(buf, *ch);
       }
       continue;
     }
 
-    mutt_buffer_addch(buf, *ch);
+    buf_addch(buf, *ch);
   }
 
-  mutt_buffer_dealloc(&buf_callback);
+  buf_dealloc(&buf_callback);
 }
 
 int main(int argc, char *argv[])
@@ -132,7 +132,7 @@ int main(int argc, char *argv[])
                            { "d", callback_animal, &animal },
                            { NULL } };
 
-  struct Buffer buf = mutt_buffer_make(1024);
+  struct Buffer buf = buf_make(1024);
 
   const char *fmt = "pre %a %b %x %c %d post";
   expando(&buf, fmt, (struct Option *) &opts);
@@ -140,6 +140,6 @@ int main(int argc, char *argv[])
   printf("format: %s\n", fmt);
   printf("result: %s\n", buf.data);
 
-  mutt_buffer_dealloc(&buf);
+  buf_dealloc(&buf);
   return 0;
 }
